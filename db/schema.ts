2e2bdfab7cb72b2eef93b506dc74bdcafd95a796
MatheusFontenele <matheus.fontenele@icloud.com>
2024-05-22 22:21:21 -0300

    refactor: altera pra usar o yarn e cria uma migracao adicionando campo plaid

--- db/schema.ts
+++ db/schema.ts
@@ -2,6 +2,7 @@ import { pgTable, text } from "drizzle-orm/pg-core";
 
 export const accounts = pgTable("accounts", {
   id: text('id').primaryKey(),
-  userId: text('user_id'),
   name: text('name'),
+  userId: text('user_id'),
+  pladeId: text('plade_id'),
 });
